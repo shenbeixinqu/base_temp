622d57301e65635d321a341eb8eca15fdacb85b7
haveadreammm <1090990640@qq.com>
2024-01-10 16:49:33 +0800

    主题

---
+++ web/src/uni/layouts/LayoutVertical/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layout-vertical" :class="{ fixed: fixedHeader }">
+    <uni-side-bar />
+    <div v-if="!collapse" @click="handleFoldSideBar"></div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "LayoutVertical",
+  props: {
+    collapse: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+    fixedHeader: {
+      type: Boolean,
+      default() {
+        return true;
+      },
+    },
+    showTabs: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      handleFoldSideBar: "settings/foldSideBar"
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
