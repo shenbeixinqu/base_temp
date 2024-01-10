0a68529ab48c5ea91d31016546e9c396aefa11a1
haveadreammm <1090990640@qq.com>
2024-01-10 16:49:03 +0800

    通用组件

---
+++ web/src/uni/layouts/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <component
+      :is="'layout-' + theme.layout"
+      :collapse="collapse"
+      :fixed-header="theme.fixedHeader"
+      :show-tabs="theme.showTabs"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  name: "Layouts",
+  data() {
+    return {
+      oldLayout: "",
+    };
+  },
+  computed: {
+    ...mapGetters({
+      collapse: "settings/collapse",
+      theme: "settings/theme"
+    })
+  },
+  beforeMount() {
+    this.oldLayout = this.theme.layout
+  },
+};
+</script>
+
+<style>
+</style>
