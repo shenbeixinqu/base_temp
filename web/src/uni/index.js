c5b25c6cb6089ba26a584a8ca8f2a7059ca50ab8
haveadreammm <1090990640@qq.com>
2024-01-10 16:50:57 +0800

    额外组件

---
+++ web/src/uni/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+
+// 加载插件
+const Plugins = require.context('./plugins', false, /\.js$/);
+Plugins.keys().map(Plugins)
+
+//加载组件
+const Components = require.context('.', true, /\.vue$/)
+Components.keys()
+  .map(Components)
+  .forEach((item) => {
+    if (item.default.name && item.default.name !== 'Layouts') {
+      Vue.component(item.default.name, item.default)
+    }
+  })
