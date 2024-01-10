88a7ddce0ae35a473226f78f2c1c45b1ae0db7b3
haveadreammm <1090990640@qq.com>
2024-01-10 16:49:46 +0800

    菜单

---
+++ web/src/uni/components/UniMenu/components/UniMenuItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-menu-item :index="itemOrMenu.path" @click="handleLink">
+    <span :title="itemOrMenu.meta.title">
+      {{ itemOrMenu.meta.title }}
+    </span>
+    <el-tag
+      v-if="itemOrMenu.meta && itemOrMenu.meta.badge"
+      effect="dark"
+      type="danger"
+    >
+      {{ itemOrMenu.meta.badge }}
+    </el-tag>
+    <span
+      v-if="itemOrMenu.meta && itemOrMenu.meta.dot"
+      class="uni-dot uni-dot-error"
+    >
+      <span />
+    </span>
+  </el-menu-item>
+</template>
+
+<script>
+import { routerMode } from "@/config";
+import { isExternal } from "@/utils/validate";
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "UniMenuItem",
+  props: {
+    itemOrMenu: {
+      type: Object,
+      default() {
+        return null;
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
