22b098168f52ac70bbfa8bdb6838bed15dfc18df
haveadreammm <1090990640@qq.com>
2024-01-11 17:04:16 +0800

    store

--- web/src/store/modules/routes.js
+++ web/src/store/modules/routes.js
@@ -1,3 +1,7 @@
+import { asyncRoutes, constantRoutes, resetRouter } from "@/router";
+import { filterRoutes } from "@/utils/routes";
+import { rolesControl } from "@/config";
+
 const state = () => ({
   routes: []
 })
@@ -7,11 +11,23 @@ const getters = {
 }
 
 const mutations = {
-
+  setRoutes(state, routes) {
+    state.routes = routes
+  }
 }
 
 const actions = {
-
+  async setRoutes({ commit }, mode = "none") {
+    let routes = [...asyncRoutes]
+    // 设置游客路由关闭路由拦截
+    const control = mode === "visit" ? false : rolesControl;
+    // 根据权限和rolesControl过滤路由
+    const accessRoutes = filterRoutes([...constantRoutes, ...routes], control);
+    // 设置菜单所需路由
+    commit("setRoutes", JSON.parse(JSON.stringify(accessRoutes)));
+    // 根据可访问路由重置Vue Router
+    await resetRouter(accessRoutes);
+  }
 }
 
 export default { state, getters, mutations, actions }
