3370e38ac594122dee1e3c065b029547266430d9
haveadreammm <1090990640@qq.com>
2024-01-11 11:42:38 +0800

    scss

---
+++ web/vue.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  css: {
+    requireModuleExtension: true,
+    sourceMap: true,
+    loaderOptions: {
+      scss: {
+        additionalData(content, loaderContext) {
+          const { resourcePath, rootContext } = loaderContext;
+          const relativePath = path.relative(rootContext, resourcePath);
+          if (
+            relativePath.replace(/\\/g, "/") !==
+            "src/uni/styles/variables/variables.scss"
+          )
+            return (
+              '@import "~@/uni/styles/variables/variables.scss";' + content
+            );
+          return content;
+        },
+      },
+    },
+  }
+}
