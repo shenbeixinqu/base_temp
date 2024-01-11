889e992ceff59c879255bcab0d838c0eb3bce911
haveadreammm <1090990640@qq.com>
2024-01-11 11:43:21 +0800

    折叠

---
+++ web/src/uni/components/UniFold/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <span @click="toggleCollapse">关闭</span>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "UniFold",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      collapse: "settings/collapse"
+    })
+  },
+  methods: {
+    ...mapActions({
+      toggleCollapse: "settings/toggleCollapse"
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
