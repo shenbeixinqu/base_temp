6efe53a67ae61988d12e6a21f65fb2828839779a
haveadreammm <1090990640@qq.com>
2024-01-11 17:02:23 +0800

    setting

--- web/src/config/setting.config.js
+++ web/src/config/setting.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   // 路由模式
   routerMode: "hash",
+  // 是否开启roles字段进行角色权限控制(如果是all模式后端完全处理角色并进行json组装，可设置false不处理路由中的roles字段)
+  rolesControl: true,
   // 布局时是否只保持一个子菜单的展开
   uniqueOpened: false,
   // 布局时默认展开的菜单
